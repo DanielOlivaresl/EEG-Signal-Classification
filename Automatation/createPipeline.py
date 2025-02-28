8a852f194aa31bba760b5ba8331029fe2846b187
Daniel Olivares <daniel.olivares0606@gmail.com>
2025-02-28 14:20:22 -0600

    Fixed issues where pipeline was not running properly

--- Automatation/createPipeline.py
+++ Automatation/createPipeline.py
@@ -2,37 +2,23 @@ from azureml.pipeline.steps import PythonScriptStep
 from azureml.pipeline.core import PipelineData
 from azureml.core import Dataset,Environment
 
-def create_pipeline_steps(ws, preprocessing_order):
-    env = Environment.get(ws, "AzureML-ACPT-pytorch-1.13-py38-cuda11.7-gpu")
+def create_pipeline_steps(ws, preprocessing_order,run_config):
     dataset = Dataset.get_by_name(ws, "EEG_Raw_Signal_Dataset")
-    input_data = dataset.as_named_input("raw_data")  
-
-    # Create intermediate output for aggregated data
-    aggregated_data = PipelineData("aggregated_data", datastore=ws.get_default_datastore())
-
-    # Step 1: Data Aggregation Step
-    data_aggregation_step = PythonScriptStep(
-        name="Data Aggregation",
-        script_name="aggregate_data.py",  # This script will merge multiple files into one list/array
-        arguments=["--input", input_data, "--output", aggregated_data],
-        inputs=[input_data],
-        outputs=[aggregated_data],
-        compute_target="eegCompute",
-        runconfig=None,
-        source_directory="./Automatation"
-    )
-
-    # Update input for preprocessing steps
-    previous_output = aggregated_data
-    steps = [data_aggregation_step]
+    input_data = dataset.as_named_input("raw_data").as_mount()  
+    
+    
+    
+    
+    previous_output = input_data
+    steps = []
 
     # Preprocessing steps
     methods_map = {
-        "notch_filter": "../Preprocessing/signalConditioning.py",
-        "bandpass_filter": "../Preprocessing/signalConditioning.py",
-        "mean_referencing": "../Preprocessing/signalConditioning.py",
-        "scale_signal": "../Preprocessing/signalConditioning.py",
-        "normalize": "../Preprocessing/signalConditioning.py"
+        "notch_filter": "signalConditioning.py",
+        "bandpass_filter": "signalConditioning.py",
+        "mean_referencing": "signalConditioning.py",
+        "scale_signal": "signalConditioning.py",
+        "normalize": "signalConditioning.py"
     }
 
     for method in preprocessing_order:
@@ -45,9 +31,10 @@ def create_pipeline_steps(ws, preprocessing_order):
             arguments=["--method", method, "--input", previous_output, "--output", output_data],
             inputs=[previous_output],
             outputs=[output_data],
-            compute_target="eegCompute",
-            runconfig=None,
+            compute_target="eegComputeCluster",
+            runconfig=run_config,
             source_directory="./Automatation"
+            
         )
 
         steps.append(step)
@@ -59,9 +46,9 @@ def create_pipeline_steps(ws, preprocessing_order):
         script_name="train.py",
         arguments=["--input", previous_output],
         inputs=[previous_output],
-        compute_target="eegCompute",
+        compute_target="eegComputeCluster",
         source_directory="./Automatation",
-        runconfig=None,
+        runconfig=run_config,
         allow_reuse=True
     )
 
--- Automatation/runPipeline.py
+++ Automatation/runPipeline.py
@@ -7,64 +7,46 @@ from azureml.core import ScriptRunConfig
 from azureml.pipeline.core import Pipeline, PipelineParameter
 from createPipeline import create_pipeline_steps  # Assuming you have a custom function here
 from azureml.core import ComputeTarget
+from azureml.core import Dataset,Environment
 
+import itertools
+from azureml.core import Experiment
+from azureml.pipeline.core import Pipeline
+from createPipeline import create_pipeline_steps
 import itertools
 
 # Get Workspace
 ws = Workspace.from_config()
 
-
 # List of preprocessing methods
-preprocessing_methods = ['notch_filter', 'bandpass_filter', 'mean_referencing', 'scale_signal', 'normalize']
+# preprocessing_methods = ['notch_filter', 'bandpass_filter', 'mean_referencing', 'scale_signal', 'normalize']
+preprocessing_methods = ['notch_filter']
 
 # Generate all combinations of preprocessing methods
 preprocessing_combinations = []
-preprocessing_combinations.extend(list(itertools.permutations(preprocessing_methods, len(preprocessing_methods))))
 
 for i in range(len(preprocessing_methods)):
     preprocessing_combinations.extend(list(itertools.permutations(preprocessing_methods, i + 1)))
 
-# Convert tuples to string format for AzureML
-preprocessing_combinations_str = ["-".join(comb) for comb in preprocessing_combinations]
-
-# Define hyperparameter search space
-search_space = {
-    "preprocessing_order": choice(*preprocessing_combinations_str)
-}
-
-
-preprocessing_order_param = PipelineParameter(name="preprocessing_order", default_value="notch_filter-bandpass_filter")
-
-default_order_list = preprocessing_order_param.default_value.split("-")
-
-
-# Create preprocessing steps dynamically
-
-steps = create_pipeline_steps(ws,default_order_list)
-
-#Create pipeline object 
-
-pipeline = Pipeline(workspace=ws,steps=steps)
-
-run_config = RunConfiguration()
-run_config.environment = ws.environments["AzureML-ACPT-pytorch-1.13-py38-cuda11.7-gpu"]
-
-# Use ScriptRunConfig to configure the pipeline's run configuration
-script_run_config = ScriptRunConfig(source_directory='./Automatation', script='train.py', run_config=run_config,compute_target=ComputeTarget(workspace=ws, name="eegCompute"))
-
-
-
-
-
-# Define the hyperdrive config
-hyperdrive_config = HyperDriveConfig(
-    run_config=script_run_config,
-    hyperparameter_sampling=RandomParameterSampling(search_space),
-    primary_metric_name="accuracy",
-    primary_metric_goal=PrimaryMetricGoal.MAXIMIZE,
-    max_total_runs=3
-)
-
-# Create and submit the experiment
+# Submit each pipeline run asynchronously
 experiment = Experiment(workspace=ws, name="preprocessing_experiment")
-run = experiment.submit(hyperdrive_config)
+compute_target = ws.compute_targets["eegComputeCluster"]
+
+env = Environment.get(ws, "eegEnv")
+run_config = RunConfiguration()
+run_config.environment = env
+for combination in preprocessing_combinations:
+    order_list = list(combination)
+    
+    # Create pipeline steps dynamically
+    steps = create_pipeline_steps(ws, order_list,run_config)
+    
+    # Create pipeline object
+    pipeline = Pipeline(workspace=ws, steps=steps)
+    
+    
+    run = experiment.submit(pipeline)
+    print(f"Submitted pipeline for order: {order_list} (Run ID: {run.id})")
+
+
+
